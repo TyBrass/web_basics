02c435593839025f4a79db9577cb151152031160
TyBrass <tybrass@outlook.com>
2021-05-09 08:52:47 -0500

    Added a Mozilla follow-along project

---
+++ mozilla-number-guessing/README.md
@@ -0,0 +1,5 @@
+# Browser-Based Number Guessing Game
+
+This is a short follow-along project from Mozilla's JavaScript course.
+
+The HTML doc is copied from Mozilla's GitHub. The JavaScript script is the follow-along part.
---
+++ mozilla-number-guessing/number-guessing.js
@@ -0,0 +1,20 @@
+/*
+1. Generate a random integer between 1 and 100.
+2. Record the turn number the player is on. Start it on 1.
+3. Provide the player with a way to guess what the number it.
+4. Once a guess has been submitted first record it somewhere so the user can see their previous guesses.
+5. Check whether it is the correct number.
+6. If it is correct:
+    1. Display congratulations message.
+    2. Stop the player from being able to enter more guesses.
+    3. Display control allowing the player to restart the game.
+7. If it is wrong and the player has turn left:
+    1. Tell the player they are wrong and whether the guess was too high or too low.
+    2. Allow them to enter another guess.
+    3. Increment the turn number by 1.
+8. If it is wrong and the player has no turns left.
+    1. Tell the player it is game over.
+    2. Stop the player from being able to enter more guesses.
+    3. Display control allowing the player to restart the game.
+9. Once the game restarts, make sure the game logic and UI are completely reset, then go back to step 1.
+*/
